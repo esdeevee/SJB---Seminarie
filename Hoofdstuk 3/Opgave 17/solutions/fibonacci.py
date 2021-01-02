985aeed61631164a656bf0315b47b72c00af60e6
esdeevee <76687782+esdeevee@users.noreply.github.com>
2021-01-02 12:02:22 +0100

    sdq

--- Hoofdstuk 3/Opgave 17/solutions/fibonacci.py
+++ Hoofdstuk 3/Opgave 17/solutions/fibonacci.py
@@ -18,3 +18,35 @@ while True:
     break
 
 print(som)
+
+
+
+
+
+from random import randint
+
+def fibonacci(n):
+  som = 0
+  a = 1
+  b = 1
+  #print(a)
+  #print(b)
+
+  while True:
+    c = a+b
+    if c < n:
+      a = b
+      b = c
+      # print(c)
+      if(c%2 == 0):
+        som = som + c
+    else:
+      break
+
+  return(som)
+
+# print(fibonacci(80))
+
+for i in range(10):
+  n = randint(1000,1000000)
+  print(n, fibonacci(n))
